314dcb28b2aab4dc2b84db5bab9ce7f338ecfb13
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-06-10 16:23:53 +0600

    project added

--- src/Page/Home/Projects.js
+++ src/Page/Home/Projects.js
@@ -4,37 +4,45 @@ import marriage from '../../Image/Project/marriage.png'
 import photography from '../../Image/Project/photography.png'
 
 const Projects = () => {
-    const [project, setProject] = useState([])
+    const [projects, setProjects] = useState([])
 
     useEffect(() => {
         fetch('project.json')
             .then(res => res.json())
-            .then(data => setProject(data))
+            .then(data => setProjects(data))
     }, [])
 
-    console.log(project)
-    const { name, img, description, technology, live, client, server, codeLink }
+    console.log(projects)
+    // const [ name, img, description, technology, live, client, server, codeLink ] = project;
 
 
     return (
         <div className='mt-36'>
-            <h1 className='text-5xl text-white text-center mb-6'>Project</h1>
-            <div className='grid grid-cols-1 lg:grid-cols-4'>
-                <div className="card p-3 w-96 bg-base-100 shadow-xl">
-                    <div class="h-96 scroll-smooth hover:scroll-auto carousel carousel-vertical rounded-box">
-                        <div class="carousel-item">
-                            <img src={img} className='w-full round' />
-                        </div>
-                    </div>
+            <h1 className='text-5xl text-white text-center mb-6'>Project{projects.length}</h1>
 
-                    <div className="card-body text-center">
-                        <h2 className="text-2xl font-bold">Dastiny Photo Graphy</h2>
-                        <p>This is a photographer protfolio. </p>
-                        <p className='text-xl'><strong>Technology :</strong> React, Firebase, Bootstrap, Javascript</p>
-                    </div>
-                </div>
+            <div className='grid grid-cols-1 w-3/4 mx-auto gap-16 lg:grid-cols-2'>
+
+
+                {
+                    projects && projects.map(project =>
+                            <div className="card  p-3 w-full bg-[#212129] shadow-xl">
+                                <div class="h-96 scroll-smooth hover:scroll-auto carousel carousel-vertical rounded-box">
+                                    <div class="carousel-item">
+                                        <img src={project.img} className='w-full round' />
+                                    </div>
+                                </div>
+
+                                <div className="card-body text-white text-center">
+                                    <h2 className="text-2xl font-bold">{project.name}</h2>
+                                    <p>{project.description}</p>
+                                    <p className='text-xl'><strong>Technology :</strong> {project.technology}</p>
+                                    <button class="btn glass text-white"><a href={project.live} target='_blank'>Live</a></button>
+                                </div>
+                            </div>
+                    )
+                }
+
             </div>
-
         </div>
     );
 };
